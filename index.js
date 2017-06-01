621949fffaa0f716a0f4f81026259434da1c3ae5
Brandon Farmer <bthesorceror@gmail.com>
2017-05-31 21:07:15 -0600

    Compute best market for trading

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-// const BTCe = require('./lib/btce')
-// const Poloniex = require('./lib/poloniex')
-const Client = require('./lib/client')
+const CurrencyConvertor = require('./lib/currency_convertor')
 
-let client = new Client()
 let from = 'btc'
 let to = ['ltc', 'eth', 'dsh']
+let convertor = new CurrencyConvertor(from, to)
 
-client.getCurrentConversions(from, to, (err, data) => {
-  if (err) {
-    return console.error(err)
-  }
+convertor.getCurrent((err, data) => {
+  if (err) { return console.error(err) }
 
-  console.log(data)
+  console.dir(data)
+  console.dir(data.all)
 })
