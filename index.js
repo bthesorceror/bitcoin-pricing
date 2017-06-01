d01aebca93c9658477fea13044205d94c3fed41a
Brandon Farmer <bthesorceror@gmail.com>
2017-05-31 20:22:14 -0600

    Consolidate results in Client class

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // const BTCe = require('./lib/btce')
-const Poloniex = require('./lib/poloniex')
+// const Poloniex = require('./lib/poloniex')
+const Client = require('./lib/client')
 
-let client = new Poloniex()
+let client = new Client()
 let from = 'btc'
 let to = ['ltc', 'eth', 'dsh']
 
